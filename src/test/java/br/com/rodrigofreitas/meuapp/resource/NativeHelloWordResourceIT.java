b3f78a5e2f43cbe89b5621025dc3b4d6a0d6fec5
Rodrigo Freitas <rodrigo.freitas@tecsinapse.com.br>
2019-07-30 19:26:17 -0300

    init

---
+++ src/test/java/br/com/rodrigofreitas/meuapp/resource/NativeHelloWordResourceIT.java
@@ -0,0 +1,9 @@
+package br.com.rodrigofreitas.meuapp.resource;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeHelloWordResourceIT extends HelloWordResourceTest {
+
+    // Execute the same tests but in native mode.
+}
